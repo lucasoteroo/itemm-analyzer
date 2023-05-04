9afd4f248afbefa0627529294678399618b60de1
netuh <waldemar.neto@gmail.com>
2023-05-04 13:03:46 -0300

    with autentication and pages

--- app.py
+++ app.py
@@ -1,35 +1,66 @@
 import os
 
-from flask import Flask, render_template_string
-from flask_security import Security, current_user, auth_required, \
-    hash_password, SQLAlchemySessionUserDatastore
+from flask import Flask, render_template
+from flask_security import (
+    Security,
+    current_user,
+    auth_required,
+    hash_password,
+    SQLAlchemySessionUserDatastore,
+)
 from database import db_session, init_db
 from models.auth import User, Role
 
 # Create app
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config["DEBUG"] = True
 
 # Generate a nice key using secrets.token_urlsafe()
-app.config['SECRET_KEY'] = os.environ.get(
-    "SECRET_KEY", 'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')
+app.config["SECRET_KEY"] = os.environ.get(
+    "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
+)
 # Bcrypt is set as default SECURITY_PASSWORD_HASH, which requires a salt
 # Generate a good salt using: secrets.SystemRandom().getrandbits(128)
-app.config['SECURITY_PASSWORD_SALT'] = os.environ.get(
-    "SECURITY_PASSWORD_SALT", '146585145368132386173505678016728509634')
+app.config["SECURITY_PASSWORD_SALT"] = os.environ.get(
+    "SECURITY_PASSWORD_SALT", "146585145368132386173505678016728509634"
+)
 
 # Setup Flask-Security
 user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
 app.security = Security(app, user_datastore)
 
+
 # Views
+@app.route("/dac")
+@auth_required()
+def dac():
+    return render_template("dac.html")
+
+
+@app.route("/polarizacao")
+@auth_required()
+def polarizacao():
+    return render_template("polarizacao.html")
+
+
+@app.route("/pekeurt")
+@auth_required()
+def pekeurt():
+    return render_template("pekeurt.html")
+
+
+@app.route("/consumo")
+@auth_required()
+def consumo():
+    return render_template("consumo.html")
 
 
 @app.route("/")
+@app.route("/home")
 @auth_required()
 def home():
-    return render_template_string('Hello {{email}} !',
-                                  email=current_user.email)
+    return render_template("index.html", name=current_user.email)
+    # return render_template_string("Hello {{email}} !", email=current_user.email)
 
 
 # one time setup
@@ -38,10 +69,12 @@ with app.app_context():
     init_db()
     if not app.security.datastore.find_user(email="test@me.com"):
         app.security.datastore.create_user(
-            email="test@me.com", password=hash_password("password"))
+            email="test@me.com", password=hash_password("password")
+        )
     db_session.commit()
+    db_session.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # run application (can also use flask run)
     app.run()
 
@@ -56,8 +89,7 @@ def create_user():
         print("Passwords diferentes")
         return 1
     try:
-        app.security.datastore.create_user(
-            email=email, password=password)
+        app.security.datastore.create_user(email=email, password=password)
         db_session.commit()
         print(f"Usuario com email {email} criado com sucesso!")
     except Exception as e:
