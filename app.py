e5c02bd6b4670969e7d93679d9641375646dc88f
Lucas Sotero <105369580+lucasoteroo@users.noreply.github.com>
2023-05-18 01:53:22 +0000

    alteração final

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+from datetime import datetime, timedelta
+import pandas as pd
+import sqlite3
 
 from flask import Flask, render_template
 from flask_security import (
@@ -46,7 +49,41 @@ def polarizacao():
 @app.route("/pekeurt")
 @auth_required()
 def pekeurt():
-    return render_template("pekeurt.html")
+    
+    # Conecta ao banco de dados SQLite
+    conn = sqlite3.connect('pet.db')
+   
+    # Carrega a tabela do banco de dados em um DataFrame do pandas
+    consulta = pd.read_sql_query('SELECT * FROM dados', conn)
+
+    # Filtra as linhas que contém o valor desejado, remove as linhas com valores nulos e em que a coluna 'Current' é igual a zero, e remove valores duplicados na coluna 'Current'
+    linha_especifica = consulta.loc[(consulta['Voltage'] == '1,750') & (consulta['Current'] != 0)].dropna().drop_duplicates(subset=['Current'])[['StepTime', 'Current']]
+
+    # Converte o formato de hora para número de horas com precisão de 3 casas decimais
+    linha_especifica['StepTime'] = pd.to_timedelta(linha_especifica['StepTime']).apply(lambda x: x.total_seconds() / 3600).round(3)
+
+    #print("Valores Peukert para o Am 01 (CBI22-076), sendo StepTime(duração) e Current(corrente)")
+    #print(" ")
+
+    # Imprime as linhas selecionadas
+    #print(linha_especifica)
+
+    #-------------------------------------------------------
+
+    # Carrega a tabela do banco de dados em um DataFrame do pandas
+    consulta2 = pd.read_sql_query('SELECT * FROM peukert_dados', conn)
+
+    # Filtra as linhas que contém o valor desejado, remove as linhas com valores nulos e em que a coluna 'Current' é igual a zero, e remove valores duplicados na coluna 'Current'
+    linha_especifica2 = consulta2.loc[(consulta2['Voltage'] == '1,75') & (consulta2['Current'] != 0)].dropna().drop_duplicates(subset=['Current'])[['StepTime', 'Current']]
+
+    # Converte o formato de hora para número de horas com precisão de 3 casas decimais
+    linha_especifica2['StepTime'] = pd.to_timedelta(linha_especifica2['StepTime']).apply(lambda x: x.total_seconds() / 3600).round(3)
+
+    # Imprime as linhas selecionadas
+    #print(linha_especifica2)
+    conn.close()
+    return render_template("pekeurt.html", consulta1=linha_especifica, consulta2=linha_especifica2)
+    
 
 
 @app.route("/consumo")
